13e0d16de066d1970e4ab3a42945554a190ba723
bezbrain <belloquadriolawale@gmail.com>
2024-01-13 13:22:41 +0100

    Create a template for upload of files to the server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [isUpload, setIsUpload] = useState();
+
+  const handleFileChange = (e) => {
+    e.preventDefault();
+    if (e.target.files) {
+      // console.log(e.target.files);
+      const fileDetails = e.target.files[0]; // This is to extract the single object from the array
+      setIsUpload(fileDetails);
+    }
+  };
+
+  const uploadFile = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("endpoint-url", isUpload);
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="App">
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={uploadFile}>UPLOAD</button>
+    </form>
   );
 }
 
